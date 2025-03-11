b285feb1d390f91c83d6cdc90fb7eaea26278c6c
Ryan Nixon <r.taiidani@gmail.com>
2025-03-11 00:31:28 -0700

    Populate existing NULLs with empty strings

--- internal/db/migrations/20250311071943_not_null_defaults.sql
+++ internal/db/migrations/20250311071943_not_null_defaults.sql
@@ -1,4 +1,6 @@
 -- +goose Up
+UPDATE item SET quantity = '' WHERE quantity IS NULL;
+UPDATE category SET description = '' WHERE description IS NULL;
 ALTER TABLE item ALTER COLUMN quantity SET DEFAULT '';
 ALTER TABLE category ALTER COLUMN description SET DEFAULT '';
 ALTER TABLE category ALTER COLUMN description SET NOT NULL;
