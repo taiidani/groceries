9b028c33dadd921d19f9c79c6d4bd6fc7089e114
Ryan Nixon <r.taiidani@gmail.com>
2025-04-19 13:01:43 -0700

    Add client-side search filter

--- internal/server/assets/index.js
+++ internal/server/assets/index.js
@@ -19,3 +19,35 @@ document.querySelectorAll("dialog").forEach(function (dialog) {
         dialog.close()
     })
 })
+
+// Search forms
+document.querySelectorAll("[role=search]").forEach(function (form) {
+    form.addEventListener("submit", function (evt) {
+        evt.preventDefault()
+    })
+
+    form.addEventListener("keyup", function (evt) {
+        let target = form.getAttribute("search-target")
+        let search = form.elements["search"].value.toLowerCase()
+
+        console.log(target)
+        console.log(search)
+
+        if (search === "") {
+            document.querySelectorAll(target + " .item").forEach(function (n) {
+                n.classList.remove("hide")
+            })
+            return
+        }
+
+        document.querySelectorAll(target + " .item .name").forEach(function (n) {
+            let content = n.textContent.toLowerCase()
+
+            if (content.indexOf(search) === -1) {
+                n.closest(".item").classList.add("hide")
+            } else {
+                n.closest(".item").classList.remove("hide")
+            }
+        })
+    })
+})
