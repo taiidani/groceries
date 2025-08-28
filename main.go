fcf9cf748630f82ee3d3b8762f7777dcc267d9ba
Ryan Nixon <r.taiidani@gmail.com>
2025-08-27 23:19:23 -0700

    Enable Sentry Logs

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/getsentry/sentry-go"
+	sentryslog "github.com/getsentry/sentry-go/slog"
 	"github.com/go-redis/redis/v8"
 	"github.com/taiidani/groceries/internal/cache"
 	"github.com/taiidani/groceries/internal/models"
@@ -24,16 +25,20 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	defer cancel()
 
-	teardown, err := initSentry()
+	// Set up Sentry
+	err := sentry.Init(sentry.ClientOptions{
+		SampleRate:       1.0,
+		EnableTracing:    true,
+		TracesSampleRate: 1.0,
+		EnableLogs:       true,
+	})
 	if err != nil {
 		log.Fatalf("sentry.Init: %s", err)
 	}
-
-	// Flush buffered Sentry events before the program terminates.
-	defer teardown()
+	defer sentry.Flush(2 * time.Second)
 
 	// Set up the structured logger
-	initLogging()
+	initLogging(ctx)
 
 	// Set up the Redis/Memory database
 	rds := cache.NewClient(ctx)
@@ -41,6 +46,7 @@ func main() {
 	// Set up the relational database
 	err = models.InitDB(ctx)
 	if err != nil {
+		sentry.CaptureException(err)
 		log.Fatalf("database init: %s", err)
 	}
 
@@ -52,55 +58,53 @@ func main() {
 		defer wg.Done()
 		// Start the web UI
 		if err := initServer(ctx, rds); err != nil {
+			sentry.CaptureException(err)
 			log.Fatal(err)
 		}
 	}()
 
 	wg.Wait()
 
-	fmt.Println("Shutdown successful")
+	slog.Info("Shutdown successful")
 }
 
-func initLogging() {
-	var level slog.Level
-	switch os.Getenv("LOG_LEVEL") {
-	case "error":
-		level = slog.LevelError
-	case "warn":
-		level = slog.LevelWarn
-	case "debug":
-		level = slog.LevelDebug
+func initLogging(ctx context.Context) {
+	var logger *slog.Logger
+
+	switch os.Getenv("SENTRY_ENVIRONMENT") {
+	case "prod", "production":
+		handler := sentryslog.Option{
+			// Explicitly specify the levels that you want to be captured.
+			EventLevel: []slog.Level{slog.LevelError},                                 // Captures only [slog.LevelError] as error events.
+			LogLevel:   []slog.Level{slog.LevelWarn, slog.LevelInfo, slog.LevelDebug}, // Captures remaining items as log entries.
+		}.NewSentryHandler(ctx)
+		logger = slog.New(handler)
 	default:
-		level = slog.LevelInfo
+		var level slog.Level
+		switch os.Getenv("LOG_LEVEL") {
+		case "error":
+			level = slog.LevelError
+		case "warn":
+			level = slog.LevelWarn
+		case "debug":
+			level = slog.LevelDebug
+		default:
+			level = slog.LevelInfo
+		}
+
+		handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+			Level: level,
+		})
+		logger = slog.New(handler)
 	}
 
-	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-		Level: level,
-	})
-	logger := slog.New(handler)
 	slog.SetDefault(logger)
-}
-
-func initSentry() (func(), error) {
-	// Set up Sentry
-	err := sentry.Init(sentry.ClientOptions{
-		SampleRate:       1.0,
-		EnableTracing:    true,
-		TracesSampleRate: 1.0,
-	})
-	if err != nil {
-		return func() {}, err
-	}
-
-	return func() {
-		sentry.Flush(2 * time.Second)
-	}, nil
 }
 
 func initServer(ctx context.Context, rds *redis.Client) error {
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("Required PORT environment variable not present")
+		return fmt.Errorf("required PORT environment variable not present")
 	}
 
 	srv := server.NewServer(ctx, rds, port)
